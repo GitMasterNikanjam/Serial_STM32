0aa6b62d49e0bfb81df8e95da43b506c8ba90572
Mohammad Nikanjam <md.nikanjam@gmail.com>
2024-12-14 08:16:04 +0330

    update

--- Serial.h
+++ Serial.h
@@ -2,8 +2,8 @@
 #define SERIAL_H
 
 // Define the target MCU family here
-#define STM32F4
-// #define STM32F1
+// #define STM32F4
+#define STM32F1
 // #define STM32H7
 
 // ##############################################################
@@ -18,6 +18,7 @@
 #endif
 
 #include <string>
+#include <stdio.h>
 
 // ###############################################################
 
@@ -57,11 +58,54 @@ class Serial
 		
 		bool init(void);
 
+		void begin(unsigned long baud);
+    
+        void end();
+
+		int available(void);
+
+		int peek(void);
+
+		int read(void);
+
+		int availableForWrite(void);
+
+		void flush(void);
+
+		size_t write(uint8_t);
+
 		void print(std::string data);
 	
+	    void setTimeout(unsigned long timeout);  // sets maximum milliseconds to wait for stream data, default is 1 second
+  		
+		unsigned long getTimeout(void) { return _timeout; }
+
+		size_t print(const std::string &);
+		size_t print(const char[]);
+		size_t print(char);
+		size_t print(unsigned char, int);
+		size_t print(int, int);
+		size_t print(unsigned int, int);
+		size_t print(long, int);
+		size_t print(unsigned long, int);
+		size_t print(double, int = 2);
+
+		size_t println(const std::string &s);
+		size_t println(const char[]);
+		size_t println(char);
+		size_t println(unsigned char, int);
+		size_t println(int, int);
+		size_t println(unsigned int, int);
+		size_t println(long, int);
+		size_t println(unsigned long, int);
+		size_t println(double, int = 2);
+		size_t println(void);
+
 	private:
 		
 		UART_HandleTypeDef *_huart;
+
+		unsigned long _timeout;
 
 		bool _checkParameters(void);
 	
--- examples/SimpleTest_STM32F103C8t6/Core/Src/main.c
+++ examples/SimpleTest_STM32F103C8t6/Core/Src/main.c
@@ -96,6 +96,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_UART_Transmit()
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
